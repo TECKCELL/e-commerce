daba2e5dfb21476ef6510c61e27daff481cb3487
TECKCELL <53048061+TECKCELL@users.noreply.github.com>
2021-04-29 16:38:45 +0200

    cart and products
    Consuming commerce.js API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react'
-//import Products from './Components/Products/Products'
-//import Navbar  from './Components/Products/NavBar/Navbar'
+import React,{useState,useEffect} from 'react'
 import {Products,Navbar} from './Components/Products'
+import {commerce} from './lib/commerce'
 const App = () => {
+
+    const [products,setProducts] = useState([])
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+
+       const {data} =  await commerce.products.list();
+       setProducts(data);
+    }
+    const fetchCart = async () =>{
+        const cart = await commerce.cart.retrieve();
+        setCart(cart);
+    }
+    const handleAddToCart = async (productId,quantity)=>{
+        const item = await commerce.cart.add(productId,quantity);
+        setCart(item.cart);
+    }
+    useEffect(()=>{
+
+        fetchProducts();
+        fetchCart();
+    },[])
+    
     return (
         <div>
-            <Navbar />
-            <Products />
+            <Navbar totalItems = {cart.total_items} />
+            <Products products = {products} onAddToCart = {handleAddToCart} />
         </div>
     )
 }
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
 import {Grid} from '@material-ui/core'
 import Product from './Product/Product'
+import useStyles from './styles'
 
-const products = [
 
-    {id:1,name:'shoes',description:'running',price:'5$',image :'https://unsplash.com/photos/164_6wVEHfI'},
-    {id:2,name:'parfums',description:'wood',price:'5$',image :'https://unsplash.com/photos/164_6wVEHfI'},
-    {id:3,name:'jackets',description:'winter',price:'5$',image :'https://unsplash.com/photos/164_6wVEHfI'},
-    {id:4,name:'jeans',description:'straight',price:'5$',image :'https://unsplash.com/photos/164_6wVEHfI'},
-]
+const Products = ({products,onAddToCart})=>{
 
-const Products = ()=>{
+    const classes = useStyles();
 
     return(
 
-        <main>
+        <main className = {classes.content}>
+        <div className = {classes.toolbar}/>
         <Grid container justify = "center" spacing = {4}>
             {products.map((product) =>(
 
                 <Grid item key={product.id} xs ={12} sm = {6} md ={4} lg ={3}>
-                     <Product product = {product} />
+                     <Product product = {product} onAddToCart = {onAddToCart} />
                 </Grid>
             )
 
