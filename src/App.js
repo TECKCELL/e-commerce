9e6ec1e43feb54c99f859dd2e6ae2b7e4d7ad77a
TECKCELL <53048061+TECKCELL@users.noreply.github.com>
2021-04-29 13:33:27 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import Products from './Components/Products/Products'
+//import Navbar  from './Components/Products/NavBar/Navbar'
+import {Products,Navbar} from './Components/Products'
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Products />
+        </div>
+    )
+}
+
+export default App
---
+++ src/Components/Products/NavBar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import{AppBar,Toolbar,IconButton,Badge,Menu,MenuItem,Typography} from '@material-ui/core'
+import {ShoppingCart} from '@material-ui/icons'
+import useStyles from './styles'
+import logo from '../../../Assets/commerce.png'
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <AppBar position='fixed' className={classes.appBar} color="inherit">
+                 <Toolbar>
+                     <Typography variant="h6" className ={classes.title} color = 'inherit'>
+                         <img src= {logo} alt="commerce.js" height='25px'className={classes.image}/>
+                     </Typography>
+                     <div className = {classes.grow}/>
+                     <div className = {classes.button}>
+
+                        <IconButton aria-label="show cart items" color='inherit'>
+                            <Badge badgeContent ={4} color='secondary'>
+                                <ShoppingCart />
+                            </Badge>
+
+                        </IconButton>
+
+                     </div>
+                 </Toolbar>
+
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar
